0434665233858388b0186e5374494dc7a7f3ff11
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-10-23 02:23:48 +0530

    configure webclient

--- order/src/main/java/com/order/order/config/WebClientConfig.java
+++ order/src/main/java/com/order/order/config/WebClientConfig.java
@@ -10,4 +10,14 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder().build();
     }
+
+    @Bean
+    public WebClient inventoryWebClient() {
+        return WebClient.builder().baseUrl("http://localhost:8081/api/v1").build();
+    }
+
+    @Bean
+    public WebClient productWebClient() {
+        return WebClient.builder().baseUrl("http://localhost:8083/api/v1").build();
+    }
 }
