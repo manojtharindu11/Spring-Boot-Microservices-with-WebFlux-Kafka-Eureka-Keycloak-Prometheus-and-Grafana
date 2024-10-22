2473e5f89a779402986e3e974b7adcfd9b25e9e3
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-10-22 20:42:08 +0530

    create inventory microservice

---
+++ inventory/src/main/java/com/inventory/inventory/model/Inventory.java
@@ -0,0 +1,21 @@
+package com.inventory.inventory.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Inventory {
+    @Id
+    @GeneratedValue
+    private int id;
+    private int itemId;
+    private int productId;
+    private int quantity;
+}
