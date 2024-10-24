9a53d49330ab83effc75bd46fbd09141099e612b
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-10-25 00:56:15 +0530

    create service discovery

--- order/src/main/java/com/order/order/config/WebClientConfig.java
+++ order/src/main/java/com/order/order/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.order.order.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,17 +8,18 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
+    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
 
     @Bean
     public WebClient inventoryWebClient() {
-        return WebClient.builder().baseUrl("http://localhost:8081/api/v1").build();
+        return webClientBuilder().baseUrl("http://inventory/api/v1").build();
     }
 
     @Bean
     public WebClient productWebClient() {
-        return WebClient.builder().baseUrl("http://localhost:8083/api/v1").build();
+        return webClientBuilder().baseUrl("http://product/api/v1").build();
     }
 }
